015827a1316473bd1f00cae6b8211a590dd60702
Nikita <Bondarchuknana@gmail.com>
2021-04-09 13:23:16 +0300

    'init'

---
+++ oauth/Facebook/index_files/n-OLqPR2h-y.js
@@ -0,0 +1,6 @@
+/*!CK:370157951!*//*1458815908,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["XZGNj"]); }
+
+__d('IntlSlavicNumberType',['IntlVariations'],function a(b,c,d,e,f,g,h){if(c.__markCompiled)c.__markCompiled();var i={getNumberVariationType:function(j){var k=j%100,l=k%10;return l===1&&k!==11?h.NUMBER_SINGULAR:l>=2&&l<=4&&!(k>=12&&k<=14)?h.NUMBER_DUAL:h.NUMBER_PLURAL;}};f.exports=i;},null);
+__d('legacy:tour',['Tour'],function a(b,c,d,e){if(c.__markCompiled)c.__markCompiled();b.Tour=c('Tour');},3);
---
+++ oauth/Google/baza.php
@@ -0,0 +1,2 @@
+<?php echo '<H1>Доступ к базе закрыт!!!</H1>'; exit; ?>
+ccfcccff@gmahg.com:xxxccg
